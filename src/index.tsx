e38b7fa9d4fde0917da2045a46b839656c031b02
youngrongoh <youngrong.oh@gmail.com>
2023-06-24 10:30:51 +0900

    feat. react-query 셋업

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from 'styled-components';
 import theme from './theme';
 
@@ -10,12 +11,15 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+const queryClient = new QueryClient();
 
 root.render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
