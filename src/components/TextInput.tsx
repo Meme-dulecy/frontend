eac39ea59589dd5dfe113600da3bbbeb085d86b6
seonghun <pch468@naver.com>
2023-06-24 22:01:01 +0900

    feat: TextInput 기능 추가

---
+++ src/components/TextInput.tsx
@@ -0,0 +1,44 @@
+import { ChangeEvent } from 'react';
+import { styled } from 'styled-components';
+
+type Props = {
+  text: string;
+  handleChange: (e: ChangeEvent<HTMLTextAreaElement>) => void;
+};
+
+export default function TextInput({ text, handleChange }: Props) {
+  return (
+    <StyledImageInput>
+      <StyledTextarea
+        value={text}
+        onChange={handleChange}
+        placeholder={`텍스트를${'\n'}입력하세요!!!`}
+      />
+    </StyledImageInput>
+  );
+}
+
+const StyledImageInput = styled.div`
+  width: 281px;
+  height: 299px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+`;
+
+const StyledTextarea = styled.textarea`
+  width: 100%;
+  height: 100%;
+  border: none;
+  resize: none;
+  outline: none;
+  color: white;
+  font-weight: bold;
+  text-align: center;
+  padding-top: 10px;
+  background-color: #d9d9d9;
+  &::placeholder {
+    color: white;
+  }
+`;
