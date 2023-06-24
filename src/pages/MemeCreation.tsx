ad65d25dad66e67fd678593ba8a6608bc16a3bbe
khkh0109 <ist200120@gmail.com>
2023-06-25 00:21:13 +0900

    refactor: 컴포넌트 분리

--- src/pages/MemeCreation.tsx
+++ src/pages/MemeCreation.tsx
@@ -1,8 +1,7 @@
-import { css, styled } from 'styled-components';
-import ImageInput from '../components/ImageInput';
-import FlipButton from '../components/FlipButton';
-import TextInput from '../components/TextInput';
 import { ChangeEvent, useState } from 'react';
+import styled from 'styled-components';
+import FlipButton from '../components/FlipButton';
+import MemeDetailCard from '../components/MemeDetailCard';
 
 export default function MemeCreation() {
   const [text, setText] = useState('');
@@ -18,49 +17,12 @@ export default function MemeCreation() {
 
   return (
     <StyledMemeCreation>
-      <StyledInputCard isImage={isImage}>
-        <ImageInput />
-        <TextInput text={text} handleChange={handleChange} />
-      </StyledInputCard>
+      <MemeDetailCard isImage={isImage} text={text} handleChange={handleChange} />
       <FlipButton handleFlipButtonClick={handleFlipButtonClick} />
     </StyledMemeCreation>
   );
 }
 
-type InputCardProps = {
-  isImage: boolean;
-};
-
 const StyledMemeCreation = styled.div`
   perspective: 1200px;
-`;
-
-const StyledInputCard = styled.div<InputCardProps>`
-  width: 281px;
-  height: 299px;
-  position: relative;
-  transform-style: preserve-3d;
-  transform-origin: center right;
-  transition: transform 1s;
-
-  & > div {
-    position: absolute;
-    top: 0;
-    left: 0;
-    backface-visibility: hidden;
-  }
-
-  & .image-input {
-    z-index: 10;
-  }
-
-  & .text-input {
-    transform: rotateY(-180deg);
-  }
-
-  ${({ isImage }) =>
-    !isImage &&
-    css`
-      transform: translateX(-100%) rotateY(-180deg);
-    `}
 `;
