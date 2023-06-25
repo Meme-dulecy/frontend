bdfbb7ee647beabac1a7978b491a42defbdc575e
Parksangwoo <psu8430@gmail.com>
2023-06-25 17:50:31 +0900

    feat: Not Found 페이지 추가

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,15 @@
+import * as S from "./styles";
+
+export const NotFound = () => {
+  return (
+    <div>
+      <S.CodeNumber>"404"</S.CodeNumber>
+      <S.NoticeText>
+        <span className="green-text">해당 페이지</span>는<br />
+        <span className="blue-text">제공</span>되지
+        <span className="pink-text">않고</span>
+        <br /> 있습니다. <span className="yellow-text">T_T</span>
+      </S.NoticeText>
+    </div>
+  );
+};
---
+++ src/pages/NotFound/styles.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const CodeNumber = styled.h1`
+  font-size: 68px;
+  line-height: 102px;
+  color: #ff99f8;
+`;
+
+export const NoticeText = styled.p`
+  font-size: 35px;
+  font-weight: 700;
+  color: #d9d9d9;
+
+  & .green-text {
+    color: #86fa16;
+  }
+  & .pink-text {
+    color: #ff99f8;
+  }
+  & .yellow-text {
+    color: #ffee00;
+  }
+  & .blue-text {
+    color: #6ffff6;
+  }
+`;
