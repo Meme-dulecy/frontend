f16bedd9e63c4992dde2599825216a977f0feeea
seonghun <pch468@naver.com>
2023-06-26 16:12:45 +0900

    feat: 라우터에 MemeDetail 추가

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,6 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { NotFound } from "../pages/NotFound";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { NotFound } from '../pages/NotFound';
 import Main from '../pages/Main';
+import MemeDetail from '../pages/MemeDetails';
 
 const ServiceRouter = () => {
   return (
@@ -10,6 +11,7 @@ const ServiceRouter = () => {
         <Route path="/meme" element={} />
         <Route path="/user" element={} /> */}
         <Route path="/" element={<Main />} />
+        <Route path="/detail" element={<MemeDetail />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
