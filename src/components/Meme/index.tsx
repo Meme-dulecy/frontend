1da520dc21f62743fba9845236e762ed86a5017c
Seonghun Kim <31424628+seonghun0828@users.noreply.github.com>
2023-06-26 16:30:54 +0900

    Revert "feat: meme 클릭하면 meme detail로 이동"
    This reverts commit 6021e9efab585eadcbada886d811b207e01329a3.

--- src/components/Meme/index.tsx
+++ src/components/Meme/index.tsx
@@ -1,10 +1,9 @@
-import React, { useMemo } from 'react';
+import React, { useMemo } from 'react'
 import Text from '../Text';
 import { DEFALUT_IMAGE } from '../../constants';
 import ProfileImage from '../ProfileImage';
 import Frame, { FloatBox, Footer, MemeFrame } from './styles';
 import { randomInt } from '../../utils/math';
-import { Link } from 'react-router-dom';
 
 interface MemeProps {
   imageURL?: string;
@@ -13,18 +12,9 @@ interface MemeProps {
   ownerProfileURL?: string;
   position: Position;
   size: Meme['size'];
-  memeId: string;
-}
+};
 
-const Meme: React.FC<MemeProps> = ({
-  imageURL,
-  text,
-  owner,
-  ownerProfileURL,
-  position,
-  size,
-  memeId,
-}) => {
+const Meme: React.FC<MemeProps> = ({ imageURL, text, owner, ownerProfileURL, position, size }) => {
   const meme = useMemo(() => {
     const [hasImage, hasText] = [imageURL != null, text != null];
     if (hasImage) {
@@ -38,18 +28,18 @@ const Meme: React.FC<MemeProps> = ({
 
   const animationDelay = useMemo(() => randomInt(1000, 0, 100), []);
   return (
-    <Link to={'/detail'} state={{ memeId }}>
-      <Frame data-ref="meme" position={position} size={size}>
-        <FloatBox delay={animationDelay}>
-          <MemeFrame>{meme}</MemeFrame>
-          <Footer>
-            <ProfileImage imageURL={ownerProfileURL} />
-            <Text size="detail">{owner}</Text>
-          </Footer>
-        </FloatBox>
-      </Frame>
-    </Link>
-  );
-};
+    <Frame data-ref="meme" position={position} size={size}>
+      <FloatBox delay={animationDelay}>
+        <MemeFrame>
+          {meme}
+        </MemeFrame>
+        <Footer>
+          <ProfileImage imageURL={ownerProfileURL} />
+          <Text size="detail">{owner}</Text>
+        </Footer>
+      </FloatBox>
+    </Frame>
+  )
+}
 
 export default Meme;
