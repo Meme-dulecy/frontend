b49f2ddd149937474f2f2f7a459001e23876e69e
Seonghun Kim <31424628+seonghun0828@users.noreply.github.com>
2023-06-26 16:30:54 +0900

    Revert "feat: meme 클릭하면 meme detail로 이동"
    This reverts commit 6021e9efab585eadcbada886d811b207e01329a3.

--- src/pages/Main/MemeList/index.tsx
+++ src/pages/Main/MemeList/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react'
 import Container from './styles';
 import Meme from '../../../components/Meme';
 import { formatMemes } from './meta';
@@ -16,13 +16,12 @@ const MemeList: React.FC<MemeListProps> = ({ memes }) => {
   useInterval(() => {
     const containerElement = containerRef.current;
     if (!containerElement) return;
-    setMemes((prev) => {
+    setMemes(prev => {
       return [...prev].map((meme) => {
-        if (!meme.position) return meme;
-        const { x, y } = meme.position;
-        return { ...meme, position: { x, y: y + 9 } };
-      });
-    });
+      if (!meme.position) return meme;
+      const { x, y } = meme.position;
+      return { ...meme, position: { x, y: y + 9 } };
+    })})
   }, 3000);
 
   useLayoutEffect(() => {
@@ -32,10 +31,8 @@ const MemeList: React.FC<MemeListProps> = ({ memes }) => {
     let memeElements: Element[] = [];
     const containerElement = containerRef.current;
     if (containerElement) {
-      memeElements = Array.from<Element>(
-        containerElement.querySelectorAll('[data-ref="meme"]')
-      );
-    }
+      memeElements = Array.from<Element>(containerElement.querySelectorAll('[data-ref="meme"]'));
+    };
     const newMemes = formatMemes(memes, memeElements);
     setMemes(newMemes);
   }, [memes, hasRenderedFirst]);
@@ -43,17 +40,12 @@ const MemeList: React.FC<MemeListProps> = ({ memes }) => {
   useLayoutEffect(() => {
     const containerElement = containerRef.current;
     if (!containerElement) return;
-    const memeElements = Array.from<Element>(
-      containerElement.querySelectorAll('[data-ref="meme"]')
-    );
-    const oldestMemeHeight =
-      memeElements.at(-1)?.getBoundingClientRect().height ?? 0;
+    const memeElements = Array.from<Element>(containerElement.querySelectorAll('[data-ref="meme"]'));
+    const oldestMemeHeight = memeElements.at(-1)?.getBoundingClientRect().height ?? 0;
     const oldestMemeY = _memes.at(-1)?.position?.y;
-    const containerHeight = oldestMemeY
-      ? oldestMemeY + oldestMemeHeight + 'px'
-      : '100vh';
+    const containerHeight = oldestMemeY ? oldestMemeY + oldestMemeHeight + 'px' : '100vh';
     containerElement.style.setProperty('height', containerHeight);
-  }, [_memes]);
+  }, [_memes])
 
   useEffect(() => {
     const containerElement = containerRef.current;
@@ -63,22 +55,13 @@ const MemeList: React.FC<MemeListProps> = ({ memes }) => {
     }, 0);
     return () => {
       clearTimeout(timer);
-    };
+    }
   }, [hasRenderedFirst]);
 
   return (
     <Container ref={containerRef} className="init">
-      {_memes.map(
-        ({
-          id,
-          owner,
-          ownerProfileURL,
-          imageURL,
-          text,
-          position = { x: 0, y: 0 },
-          createdTime,
-          size,
-        }) => (
+      {
+        _memes.map(({ id, owner, ownerProfileURL, imageURL, text, position = { x: 0, y: 0 }, createdTime, size }) => 
           <Meme
             key={id}
             imageURL={imageURL}
@@ -87,12 +70,11 @@ const MemeList: React.FC<MemeListProps> = ({ memes }) => {
             ownerProfileURL={ownerProfileURL}
             position={position}
             size={size}
-            memeId={id}
           />
         )
-      )}
+      }
     </Container>
-  );
-};
+  )
+}
 
 export default MemeList;
