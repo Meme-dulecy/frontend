fb462a52e1a22de8d69099573a5b910198cb639e
seonghun <pch468@naver.com>
2023-06-26 19:21:53 +0900

    feat: add creation-button in main page

---
+++ src/components/CreationButton.tsx
@@ -0,0 +1,21 @@
+import { styled } from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export default function CreationButton() {
+  return (
+    <Link to="/create">
+      <StyledCreationButton
+        src={`${process.env.PUBLIC_URL}/assets/images/plus-button.png`}
+        alt="creation-button"
+      />
+    </Link>
+  );
+}
+
+const StyledCreationButton = styled.img`
+  position: fixed;
+  width: 52px;
+  height: 52px;
+  bottom: 84px;
+  right: 84px;
+`;
