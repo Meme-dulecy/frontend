4cd78614fd0bc781b2fb4f4dd2a4e21374d7d494
Parksangwoo <psu8430@gmail.com>
2023-06-26 16:20:05 +0900

    feat: Add User Page

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,15 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { NotFound } from "../pages/NotFound";
-import Main from '../pages/Main';
+import Main from "../pages/Main";
+import UserPage from "../pages/User";
 
 const ServiceRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/" element={} />
-        <Route path="/meme" element={} />
-        <Route path="/user" element={} /> */}
         <Route path="/" element={<Main />} />
+        <Route path="/user" element={<UserPage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
