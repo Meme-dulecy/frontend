38aa7e18c3ac984f6be9cc060db2a3326c43de40
seonghun <pch468@naver.com>
2023-07-06 15:34:15 +0900

    style: add layout and set router

---
+++ src/components/Layout.tsx
@@ -0,0 +1,25 @@
+import { Outlet } from 'react-router-dom';
+import { styled } from 'styled-components';
+
+export default function Layout() {
+  return (
+    <FullScreen>
+      <StyledLayout>
+        <Outlet />
+      </StyledLayout>
+    </FullScreen>
+  );
+}
+
+const FullScreen = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const StyledLayout = styled.div`
+  @media screen and (max-width: 650px) {
+    width: 100%;
+  }
+  width: 375px;
+  background-color: #faffe4;
+`;
