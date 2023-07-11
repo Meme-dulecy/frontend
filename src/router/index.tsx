ba631efc245f498b8e2f7b7efbd6054b240950a8
Parksangwoo <psu8430@gmail.com>
2023-07-11 14:28:23 +0900

    Feat: Add redirect page for user login

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,11 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { NotFound } from '../pages/NotFound';
-import Main from '../pages/Main';
-import UserPage from '../pages/User';
-import MemeDetail from '../pages/MemeDetails';
-import MemeCreation from '../pages/MemeCreation';
-import Layout from '../components/Layout';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { NotFound } from "../pages/NotFound";
+import Main from "../pages/Main";
+import UserPage from "../pages/User";
+import MemeDetail from "../pages/MemeDetails";
+import MemeCreation from "../pages/MemeCreation";
+import Layout from "../components/Layout";
+import LoginCallBack from "../pages/LoginCallback";
 
 const ServiceRouter = () => {
   return (
@@ -12,6 +13,7 @@ const ServiceRouter = () => {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Main />} />
+          <Route path="/redirect" element={<LoginCallBack />} />
           <Route path="/user" element={<UserPage />} />
           <Route path="/detail" element={<MemeDetail />} />
           <Route path="/create" element={<MemeCreation />} />
