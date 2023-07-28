833a4b2eff1536c385b4d386f71348d79d463e72
khkh0109 <ist200120@gmail.com>
2023-07-29 00:16:48 +0900

    design: StartButton에 배경 추가

--- src/components/Onboarding/StartButton.tsx
+++ src/components/Onboarding/StartButton.tsx
@@ -5,16 +5,40 @@ interface StartButtonProps {
 }
 
 function StartButton({ handleStartButtonClick }: StartButtonProps) {
-  return <Button onClick={handleStartButtonClick}>시작하기</Button>;
+  return (
+    <Background>
+      <Button onClick={handleStartButtonClick}>시작하기</Button>
+    </Background>
+  );
 }
 
+const Background = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  width: 100%;
+  height: 120px;
+  padding: 0 20px 20px 20px;
+  position: absolute;
+  bottom: 0;
+  background-color: #fff;
+  background: linear-gradient(
+    180deg,
+    rgba(255, 255, 255, 0) 0%,
+    rgba(255, 255, 255, 0.05) 3.65%,
+    rgba(255, 255, 255, 0.45) 10.42%,
+    rgba(255, 255, 255, 0.8) 16.67%,
+    rgba(255, 255, 255, 0.9) 19.79%,
+    #fff 25%,
+    #fff 100%
+  );
+`;
+
 const Button = styled.button`
-  width: calc(100% - 20px);
+  width: 100%;
   height: 48px;
   border: none;
   border-radius: 12px;
-  position: absolute;
-  bottom: 10px;
   font-size: 18px;
   font-weight: 700;
   line-height: 48px;
