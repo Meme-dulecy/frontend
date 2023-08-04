2e1145298da7ba1ebf5c3f6be9b4a17c042ae6a8
seonghun <pch468@naver.com>
2023-08-04 21:37:06 +0900

    feat: StrictMode 제거

--- src/index.tsx
+++ src/index.tsx
@@ -16,17 +16,15 @@ const root = ReactDOM.createRoot(
 const queryClient = new QueryClient();
 
 root.render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <SocketProvider>
-        <CookiesProvider>
-          <ThemeProvider theme={theme}>
-            <App />
-          </ThemeProvider>
-        </CookiesProvider>
-      </SocketProvider>
-    </QueryClientProvider>
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <SocketProvider>
+      <CookiesProvider>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </CookiesProvider>
+    </SocketProvider>
+  </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
