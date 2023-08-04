8399f8b477c658346105d111b7a933cde4c842a8
seonghun <pch468@naver.com>
2023-08-04 23:36:42 +0900

    feat: 주요 속성 추가 및 코드 가독성 개선

--- src/pages/Main/MemeList/meta.ts
+++ src/pages/Main/MemeList/meta.ts
@@ -1,54 +1,86 @@
-import { MEME_WIDTH_BY_SIZE } from '../../../constants';
+import { MEME_WIDTH_BY_SIZE } from "../../../constants";
 
-export const byCreatedTime = (a: Meme, b: Meme) => b.createdTime - a.createdTime;
+export const byCreatedTime = (a: Meme, b: Meme) =>
+  b.createdTime - a.createdTime;
 
-export const formatMemes = (memes: Meme[], memeElements: Element[]) => {
-  const memesWithElements = memes.map((meme, i) => ({ ...meme, element: memeElements[i] }));
-  return memesWithElements.sort(byCreatedTime).map((meme, _, memes) => {
-    return {
-      ...meme,
-      position: findPosition(meme, memes),
-    };
-  });
+export const formatMemes = (
+  memes: Meme[],
+  memeElements: Element[],
+  containerWidth: number
+) => {
+  const memesWithElements = memes.map((meme, i) => ({
+    ...meme,
+    element: memeElements[i],
+  }));
+
+  return memesWithElements.sort(byCreatedTime).map((meme, _, memes) => ({
+    ...meme,
+    position: findPosition(meme, memes, containerWidth),
+  }));
 };
-type MemeWithElement = Meme & { element: Element };
-const SCROLLBAR_WIDTH = 15;
-export const findPosition = (target: MemeWithElement, memes: MemeWithElement[]): Position => {
-  const viewportWidth = window.innerWidth === document.body.scrollWidth ? window.innerWidth : window.innerWidth - SCROLLBAR_WIDTH;
-  const x = arrangeXCoordinate(target.createdTime % viewportWidth, MEME_WIDTH_BY_SIZE[target.size], viewportWidth, memes);
-  const y = arrangeYCoordinate(target);
-  return { x, y };
-}
 
-const RETRY_COUNT_LIMIT = 5;
-const arrangeXCoordinate = (left: number, width: number, max: number, memes: Meme[], retryCount = 0): number => {
-  const right = left + width;
-  const overlapsViewportRight = right > max;
-  const overlapsOthers = memes.some(({ position, size }) => 
-    position
-    && (left < position.x + MEME_WIDTH_BY_SIZE[size] || right > position.x)
+type MemeWithElement = Meme & { element: Element };
+
+export const findPosition = (
+  target: MemeWithElement,
+  memes: MemeWithElement[],
+  containerWidth: number
+): Position => {
+  const x = arrangeXCoordinate(
+    target.createdTime % containerWidth,
+    MEME_WIDTH_BY_SIZE[target.size],
+    containerWidth,
+    memes
   );
 
-  const overlaps = overlapsOthers || overlapsViewportRight;
-  if (overlaps && retryCount < RETRY_COUNT_LIMIT) {
-    let jumpSize = Math.floor(left / 2);
-    if (jumpSize === 0) jumpSize = width;
+  const y = arrangeYCoordinate(target);
 
-    let leftToRetry = left + jumpSize;
-    if (overlapsViewportRight) {
-      leftToRetry = jumpSize;
-    }
+  return { x, y };
+};
+
+const RETRY_COUNT_LIMIT = 5;
+
+const arrangeXCoordinate = (
+  left: number,
+  width: number,
+  max: number,
+  memes: Meme[],
+  retryCount = 0
+): number => {
+  const right = left + width;
+
+  const overlapsOthers = memes.some(({ position, size }) => {
+    if (!position) return false;
+
+    const memeRight = position.x + MEME_WIDTH_BY_SIZE[size];
+
+    return left < memeRight || right > position.x;
+  });
+
+  const overRange = right > max;
+  const needArrage = overlapsOthers || overRange;
+
+  if (needArrage && retryCount < RETRY_COUNT_LIMIT) {
+    const halfLeft = Math.floor(left / 2);
+    const jumpSize = halfLeft === 0 ? width : halfLeft;
+    const leftToRetry = overRange ? left + jumpSize : jumpSize;
+
     return arrangeXCoordinate(leftToRetry, width, max, memes, retryCount + 1);
-  };
+  }
+
   return left;
 };
 
 const SEC = 1000;
 const PX_PER_UNIT_TIME = 1;
+
 const arrangeYCoordinate = (target: MemeWithElement) => {
   const currentTime = new Date().getTime();
-  const yToTime = Math.floor((currentTime - target.createdTime) / SEC) * PX_PER_UNIT_TIME;
-  if (target.element) return yToTime;
-  return 0;
-}
 
+  const yToTime =
+    Math.floor((currentTime - target.createdTime) / SEC) * PX_PER_UNIT_TIME;
+
+  if (target.element) return yToTime;
+
+  return 0;
+};
