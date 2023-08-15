757ba764b8c624d61bdf7e90fd756763f14d0190
seonghun <pch468@naver.com>
2023-08-15 23:10:46 +0900

    feat: 이차원 배열 구조에 맞게 변경

--- src/hooks/queries/useMemes.ts
+++ src/hooks/queries/useMemes.ts
@@ -9,39 +9,42 @@ const useMemes = () => {
     QUERY_KEY.MEMES,
     () =>
       new Promise((res) =>
-        socketIO.on(SOCKET_MESSAGE.FETCH_MEMES, (memesRaw: MemeRaw[]) => {
-          const memes = (memesRaw ?? []).map<Meme>((meme) => {
-            let size: keyof typeof MEME_WIDTH_BY_SIZE;
+        socketIO.on(SOCKET_MESSAGE.FETCH_MEMES, (memeRaws: MemeRaw[][]) => {
+          const memesTimeLine = (memeRaws ?? []).map<Meme[]>((memes) =>
+            memes.map((meme) => {
+              let size: keyof typeof MEME_WIDTH_BY_SIZE;
 
-            if (meme.distance < 300) {
-              size = "L";
-            } else if (meme.distance < 1000) {
-              size = "M";
-            } else {
-              size = "S";
-            }
+              if (meme.distance < 300) {
+                size = "L";
+              } else if (meme.distance < 1000) {
+                size = "M";
+              } else {
+                size = "S";
+              }
 
-            return {
-              id: meme.memeId,
-              owner: meme.nickname,
-              ownerId: meme.creator,
-              updatorId: meme.updater,
-              createdTime: meme.createdTs,
-              updatedTime: meme.updatedTs,
-              ownerProfileURL: meme.profileImg,
-              size,
-              imageURL: meme.imgUrl,
-              text: meme.message,
-              stickers: meme.stickers,
-            };
-          });
-          res(memes);
+              return {
+                id: meme.memeId,
+                owner: meme.nickname,
+                ownerId: meme.creator,
+                updatorId: meme.updater,
+                createdTime: meme.createdTs,
+                updatedTime: meme.updatedTs,
+                ownerProfileURL: meme.profileImg,
+                size,
+                imageURL: meme.imgUrl,
+                text: meme.message,
+                stickers: meme.stickers,
+              };
+            })
+          );
+
+          res(memesTimeLine);
         })
       )
   );
 
   return {
-    memes: data as Meme[],
+    memesTimeLine: data as Meme[][],
     isLoading,
   };
 };
