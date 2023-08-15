4bf9a49dfda32c543fa21e9800aefb6e14ade9a9
seonghun <pch468@naver.com>
2023-08-15 23:10:14 +0900

    style: transition 3s -> 1s로 바꿈

--- src/components/Meme/styles.tsx
+++ src/components/Meme/styles.tsx
@@ -1,24 +1,28 @@
-import styled from 'styled-components';
-import { typo, themeColor } from '../../utils/style';
-import { MEME_WIDTH_BY_SIZE } from '../../constants';
+import styled from "styled-components";
+import { typo, themeColor } from "../../utils/style";
+import { MEME_WIDTH_BY_SIZE } from "../../constants";
 
-export default styled.div.attrs<{ position: Position, size: Meme['size'] }>(({ size, position }) => ({
-  style: {
-    width: MEME_WIDTH_BY_SIZE[size] + 'px',
-    transform: `translate(${position.x}px, -${position.y}px)`,
-  },
-}))`
-  ${typo('body')}
-  color: ${themeColor('text')};
+export default styled.div.attrs<{ position: Position; size: Meme["size"] }>(
+  ({ size, position }) => ({
+    style: {
+      width: MEME_WIDTH_BY_SIZE[size] + "px",
+      transform: `translate(${position.x}px, -${position.y}px)`,
+    },
+  })
+)`
+  ${typo("body")}
+  color: ${themeColor("text")};
   border-radius: 4px;
   position: absolute;
-  transition: transform 3s linear;
+  transition: transform 1s linear;
   bottom: 0;
 `;
 
 export const FloatBox = styled.div.attrs<{ delay: number }>(({ delay }) => ({
   style: {
-    animation: `6s linear ${delay}ms infinite ${delay % 2 === 0 ? '' : 'reverse'} float`
+    animation: `6s linear ${delay}ms infinite ${
+      delay % 2 === 0 ? "" : "reverse"
+    } float`,
   },
 }))`
   background: #fff;
