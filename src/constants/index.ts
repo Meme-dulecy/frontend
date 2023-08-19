7d138d10b1a11eb93e04464f83d26e8f4c899150
seonghun <pch468@naver.com>
2023-08-19 15:32:45 +0900

    feat: 메인 화면 밈 프로필 size에 따라 다른 width를 반환하는 객체 추가

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,8 +1,14 @@
-export const DEFALUT_IMAGE = 'https://dummyimage.com/600x400/000/fff';
-export const DEFALUT__PROFILE_IMAGE = 'https://dummyimage.com/600x400/000/fff';
+export const DEFALUT_IMAGE = "https://dummyimage.com/600x400/000/fff";
+export const DEFALUT__PROFILE_IMAGE = "https://dummyimage.com/600x400/000/fff";
 
-export const MEME_WIDTH_BY_SIZE: Record<Meme['size'], number> = {
-  S: 80,
+export const MEME_WIDTH_BY_SIZE: Record<Meme["size"], number> = {
+  S: 100,
   M: 150,
   L: 200,
+};
+
+export const MEME_PROFILE_WIDTH_BY_SIZE: Record<Meme["size"], number> = {
+  S: 16,
+  M: 20,
+  L: 24,
 };
