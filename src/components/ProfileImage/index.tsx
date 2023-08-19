8ef54ebb8680d6ce5b2abd1c1410393c410d4472
seonghun <pch468@naver.com>
2023-08-19 15:36:46 +0900

    feat: ProfileImage 컴포넌트에 size 속성 추가, 적용

--- src/components/ProfileImage/index.tsx
+++ src/components/ProfileImage/index.tsx
@@ -1,18 +1,25 @@
-import React, { useMemo } from 'react'
-import { DEFALUT__PROFILE_IMAGE } from '../../constants';
-import Frame from './styles';
+import React, { useMemo } from "react";
+import {
+  DEFALUT__PROFILE_IMAGE,
+  MEME_PROFILE_WIDTH_BY_SIZE,
+} from "../../constants";
+import Frame from "./styles";
 
 interface ProfileImageProps {
   imageURL?: string;
+  size: "S" | "M" | "L";
 }
 
-const ProfileImage: React.FC<ProfileImageProps> = ({ imageURL }) => {
-  const image = useMemo(() => imageURL !== undefined ? imageURL : DEFALUT__PROFILE_IMAGE, [imageURL]);
+const ProfileImage: React.FC<ProfileImageProps> = ({ imageURL, size }) => {
+  const image = useMemo(
+    () => (imageURL !== undefined ? imageURL : DEFALUT__PROFILE_IMAGE),
+    [imageURL]
+  );
   return (
-    <Frame>
+    <Frame size={MEME_PROFILE_WIDTH_BY_SIZE[size]}>
       <img src={image} alt="user profile" />
     </Frame>
-  )
-}
+  );
+};
 
 export default ProfileImage;
