87625f829d7675d2f84e63aec49ba01f5d174071
seonghun <pch468@naver.com>
2023-08-19 15:39:00 +0900

    feat: Meme 컴포넌트에 밈사이즈 -> 타이포사이즈 대응 객체 추가, 사이즈 별로 밈 다르게 렌더링 되도록 변경

--- src/components/Meme/index.tsx
+++ src/components/Meme/index.tsx
@@ -1,10 +1,11 @@
-import React, { useMemo } from 'react';
-import Text from '../Text';
-import { DEFALUT_IMAGE } from '../../constants';
-import ProfileImage from '../ProfileImage';
-import Frame, { FloatBox, Footer, MemeFrame } from './styles';
-import { randomInt } from '../../utils/math';
-import { Link } from 'react-router-dom';
+import React, { useMemo } from "react";
+import Text from "../Text";
+import { DEFALUT_IMAGE } from "../../constants";
+import ProfileImage from "../ProfileImage";
+import Frame, { FloatBox, Footer, MemeFrame } from "./styles";
+import { randomInt } from "../../utils/math";
+import { Link } from "react-router-dom";
+import { DefaultTheme } from "styled-components/dist/types";
 
 interface MemeProps {
   imageURL?: string;
@@ -12,7 +13,7 @@ interface MemeProps {
   owner: string;
   ownerProfileURL?: string;
   position: Position;
-  size: Meme['size'];
+  size: Meme["size"];
   memeId: string;
 }
 
@@ -30,26 +31,37 @@ const Meme: React.FC<MemeProps> = ({
     if (hasImage) {
       return <img src={imageURL} alt={text} />;
     } else if (hasText) {
-      return <Text size="detail">{text}</Text>;
+      return <Text size={textSizeMap[size]}>{text}</Text>;
     } else {
       return <img src={DEFALUT_IMAGE} alt={text} />;
     }
-  }, [imageURL, text]);
+  }, [imageURL, text, size]);
 
   const animationDelay = useMemo(() => randomInt(1000, 0, 100), []);
+
   return (
-    <Link to={'/detail'} state={{ memeId }}>
+    <Link to={"/detail"} state={{ memeId }}>
       <Frame data-ref="meme" position={position} size={size}>
         <FloatBox delay={animationDelay}>
           <MemeFrame>{meme}</MemeFrame>
           <Footer>
-            <ProfileImage imageURL={ownerProfileURL} />
-            <Text size="detail">{owner}</Text>
+            <ProfileImage imageURL={ownerProfileURL} size={size} />
+            <Text size={textSizeMap[size]}>{owner}</Text>
           </Footer>
         </FloatBox>
       </Frame>
     </Link>
   );
+};
+
+const textSizeMap: {
+  L: keyof DefaultTheme["typography"];
+  M: keyof DefaultTheme["typography"];
+  S: keyof DefaultTheme["typography"];
+} = {
+  L: "detail1",
+  M: "detail2",
+  S: "detail3",
 };
 
 export default Meme;
