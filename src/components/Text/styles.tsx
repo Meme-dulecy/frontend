9c1384ad5ac8521e9730e9598dbad9ba06c4f7db
seonghun <pch468@naver.com>
2023-08-19 15:33:28 +0900

    feat: Text 컴포넌트 스타일에 detail1, 2, 3 적용

--- src/components/Text/styles.tsx
+++ src/components/Text/styles.tsx
@@ -1,33 +1,35 @@
-import styled, { DefaultTheme } from 'styled-components';
-import { typo, themeColor } from '../../utils/style';
+import styled, { DefaultTheme } from "styled-components";
+import { typo, themeColor } from "../../utils/style";
 
 export interface TextStyleProps {
-  size: keyof DefaultTheme['typography'];
+  size: keyof DefaultTheme["typography"];
   type?: keyof typeof typeMap;
-  color?: keyof DefaultTheme['color'];
-};
+  color?: keyof DefaultTheme["color"];
+}
 
 export default styled.span<TextStyleProps>`
   ${({ size }) => typo(size)};
 
   display: ${({ type, size }) => {
     if (type !== undefined) return typeMap[type];
-    switch (size){
-      case 'body':
-      case 'detail':
+    switch (size) {
+      case "body":
+      case "detail1":
+      case "detail2":
+      case "detail3":
         return typeMap.inline;
-      case 'heading1':
-      case 'heading2':
-      case 'heading3':
-      case 'heading4':
+      case "heading1":
+      case "heading2":
+      case "heading3":
+      case "heading4":
         return typeMap.block;
     }
   }};
 
-  color: ${({ color, theme }) => themeColor(color ?? 'text')({ theme })};
+  color: ${({ color, theme }) => themeColor(color ?? "text")({ theme })};
 `;
 
 const typeMap = {
-  inline: 'inline-block',
-  block: 'block',
+  inline: "inline-block",
+  block: "block",
 } as const;
